0b2d78f6859a8ca2154385cc88d43b4cffdf549e
Lucasccgomes <lucasccgomes@protonmail.com>
2024-04-01 11:27:50 -0300

    config

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,12 @@ import DownloadIcon from "./assets/baixar.png"
 
 export default function App() {
   return (
-    <div className="w-screen h-full bg-[#ffe0e8]">
+    <div className="w-full h-screen bg-[#ffe0e8]">
       <div className="flex flex-col justify-center items-center mx-8 ">
         <h1 className="text-2xl font-bold text-center text-amber-950 mt-8">
           Bem-vindo à Página de Download do App Doce Sabor
         </h1>
-        <img  src={Logo} alt="Logo" />
+        <img src={Logo} alt="Logo" />
         <div className="-mt-16">
           <h2 className="font-semibold text-center mb-3  text-amber-950">Pronto para experiência?</h2>
           <h3 className="font-medium mb-2  text-amber-950">Siga estes passos para começar:</h3>
@@ -21,9 +21,13 @@ export default function App() {
             <li className="mb-1">Abra o app, faça seu pedido e prepare-se para uma delícia!</li>
           </ol>
         </div>
-        <div className="flex bg-green-700 p-3 rounded-3xl text-white font-semibold mt-3">
-        <img  className="w-6 mr-2" src={DownloadIcon} alt="Logo" />  Download
-        </div>
+        <a href="https://drive.google.com/file/d/1PUrEHwFSSB-_wTerxabLXoXCLSgp2OuF/view?usp=sharing" target="_blank" rel="noopener noreferrer">
+          <div className="flex bg-green-700 p-3 rounded-3xl text-white font-semibold mt-3">
+            <img className="w-6 mr-2" src={DownloadIcon} alt="Download icon" />
+            Download
+          </div>
+        </a>
+
         <div className="my-4">
           <p className="font-bold ">Nota</p><p className="text-xs  text-amber-950">Este é um lançamento exclusivo e antecipado. Seu feedback é importante para nós — por favor, envie suas impressões e sugestões para</p>
         </div>
